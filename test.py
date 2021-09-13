5bc4d4f33c939a52dcb63f533646666a596ed982
Noebac <90191461+Noebac@users.noreply.github.com>
2021-09-13 13:13:05 +0200

    ✨ matte!!!

--- test.py
+++ test.py
@@ -1,3 +1,34 @@
-print ("hej på er EE20")
-print("stäng ner discord nu så fortsätter vi")
-print ("jag vill hoppa ut genom förnstet meme")
+# print ("hej på er EE20")
+# print("stäng ner discord nu så fortsätter vi")
+# print ("jag vill hoppa ut genom förnstet meme")
+
+
+#tal1 = 12
+#tal2 = 12
+
+#summa = tal1 * tal2
+#summa1 = tal2 + tal2
+#summa2 = tal1 - tal2
+#summa3 = tal1 / tal2
+
+#print (f"summan blir {summa}")
+
+#print (f"summan blir {summa1}")
+#print (f"summan blir {summa2}")
+#print (f"summan blir {summa3}")
+
+
+tal1 = 12023
+tal2 = float(input("Skriv ett tal: "))
+
+summa = tal1 * tal2
+print (f"summan av {tal1} * {tal2} blir {summa}")
+
+summa = tal1 + tal2
+print (f"summan blir {summa}")
+
+summa = tal1 - tal2
+print (f"summan blir {summa}")
+
+summa = tal1 /12 tal2
+print (f"summan blir {summa}")
